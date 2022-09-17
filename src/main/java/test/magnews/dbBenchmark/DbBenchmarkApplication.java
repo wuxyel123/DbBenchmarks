74212b09c55f76ff87b8b71c1a8f2a930fb61255
Alessandro Discalzi <alessandrodiscalzi98@gmail.com>
2022-09-17 14:31:15 +0200

    Added swagger

--- src/main/java/test/magnews/dbBenchmark/DbBenchmarkApplication.java
+++ src/main/java/test/magnews/dbBenchmark/DbBenchmarkApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
 @SpringBootApplication
+//To see swagger doc: http://localhost:8080/dbBenchmark/swagger-ui/index.html
 public class DbBenchmarkApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
